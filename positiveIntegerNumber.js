081c4492d6f9de7b03b86bb7136fee37867304b0
Rohit Pawar <103206980+rohitpawar47@users.noreply.github.com>
2022-07-31 18:18:38 +0530

    update 1.0

--- positiveIntegerNumber.js
+++ positiveIntegerNumber.js
@@ -1,13 +1,11 @@
 // "Given a positive integer NUM, find a pair of integers a, G such thatF a and b ae positiveF..
 
 function numberPair(num) {
-    if (num <= 9) {
-        console.log(`Invalid`);
-    }
+
     for (let a = 1; a <= num; a++) {
         for (let b = 1; b <= num; b++) {
             if (a * a + b * b == num && a <= b) {
-                console.log(a, b);
+                console.log(a + "," + b);
             }
         }
     }
@@ -17,3 +15,9 @@ numberPair(70);
 numberPair(25);
 numberPair(9);
 numberPair(10);
+numberPair(0);
+numberPair(2);
+numberPair(3);
+numberPair(1);
+numberPair(4);
+numberPair(5);
