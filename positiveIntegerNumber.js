b4a33683cd22fb443a3873526fafb8fe2f750014
rohitpawar47 <rohitpawar.skills@gmail.com>
2022-07-31 20:53:39 +0530

    initial update

--- positiveIntegerNumber.js
+++ positiveIntegerNumber.js
@@ -11,13 +11,4 @@ function numberPair(num) {
     }
 }
 numberPair(50);
-numberPair(70);
-numberPair(25);
-numberPair(9);
-numberPair(10);
-numberPair(0);
-numberPair(2);
-numberPair(3);
-numberPair(1);
-numberPair(4);
-numberPair(5);
+
